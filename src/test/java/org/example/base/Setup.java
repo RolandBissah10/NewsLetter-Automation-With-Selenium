d7eb680d88e008b428f5dbce5a291fc0b797b00b
RoalndBissah10 <rolandbissah10@gmail.com>
2026-02-16 22:36:11 +0000

    Updated CI test  with slack notifications

--- src/test/java/org/example/base/Setup.java
+++ src/test/java/org/example/base/Setup.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.pages.NewsLetterPage;
 import org.example.pages.SuccessPage;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
