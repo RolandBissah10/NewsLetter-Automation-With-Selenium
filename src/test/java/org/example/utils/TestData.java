2fa0c57d30d6fb100962d3ffdfd91ec1f9228491
RoalndBissah10 <rolandbissah10@gmail.com>
2026-02-18 17:32:03 +0000

    Modified TestData to fail test

--- src/test/java/org/example/utils/TestData.java
+++ src/test/java/org/example/utils/TestData.java
@@ -2,7 +2,7 @@ package org.example.utils;
 
 public class TestData {
     public static final String BASE_URL = "https://news-letter-signup-page.netlify.app/";
-    public static final String VALID_EMAIL = "francis.bissah@amalitech.com";
+    public static final String VALID_EMAIL = "francis.bissah@amalitech.test.com";
     public static final String INVALID_EMAIL = "fracis-bissah-amalitech.com";
     public static final String ERROR_MESSAGE_REQUIRED = "Valid email not required";
 }
