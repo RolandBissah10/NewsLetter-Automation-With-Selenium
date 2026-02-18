f163c264f579eb0ef477f35344c8e952bc980a7d
RoalndBissah10 <rolandbissah10@gmail.com>
2026-02-18 16:30:32 +0000

    Modified the email and slack notification

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -9,9 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-/**
- * Minimal base page: provides WebDriver and common explicit-wait helpers.
- */
+
 public abstract class BasePage {
     protected final WebDriver driver;
     protected final WebDriverWait wait;
